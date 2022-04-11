5d2e5b81f2bd1546862c804c0def91dff9afb60a
ian <ian.7.shaw@gmail.com>
2022-04-11 08:38:59 -0400

    update

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const userRoutes = require('./UserRoutes.js');
+const userRoutes = require('./userRoutes.js');
 const postRoutes = require('./post-routes');
 
 
